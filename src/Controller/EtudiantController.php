66bcabf9b288de524628d0e872c1c043fd126618
thierrysio <thierry28220@icloud.com>
2024-01-30 22:45:02 +0100

    ok

---
+++ src/Controller/EtudiantController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Etudiant;
+
+class EtudiantController extends AbstractController
+{
+    #[Route('/etudiant', name: 'app_etudiant')]
+    public function index(): Response
+    {
+        return $this->render('etudiant/index.html.twig', [
+            'controller_name' => 'EtudiantController',
+        ]);
+    }
+    #[Route('/etudiant/tableau', name: 'app_etudiant_tableau')]
+    public function tableau(): Response
+    {
+        return $this->render('etudiant/tableau.html.twig');
+    }
+
+    #[Route('/etudiant/{id}', name: 'etudiant_afficher')]
+
+    public function afficher(Etudiant $etudiant): Response
+    {
+
+        
+        if (!$etudiant) {
+            throw $this->createNotFoundException('Aucun étudiant trouvé pour l\'id '.$id);
+        }
+        
+        return $this->render('/etudiant/afficher.html.twig', [
+            'etudiant' => $etudiant,
+        ]);
+    }
+
+    #[Route('/etudiant/forme/{id}', name: 'etudiant_afficher-forme')]
+
+    public function afficherforme(Etudiant $etudiant): Response
+    {
+
+        
+        if (!$etudiant) {
+            throw $this->createNotFoundException('Aucun étudiant trouvé pour l\'id '.$id);
+        }
+        
+        return $this->render('/etudiant/afficherform.html.twig', [
+            'etudiant' => $etudiant,
+        ]);
+    }
+}
